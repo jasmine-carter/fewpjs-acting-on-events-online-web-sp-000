df1d64aa5e7442c520b2d42e4bb9085e0825b8c6
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-01 01:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Your code here
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
 
-  if (right > 0) {
-    dodger.style.right = `${left + 1}px`;
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
 
@@ -12,13 +18,7 @@ function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left > 360) {
     dodger.style.left = `${left - 1}px`;
   }
 }
-
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
-});
