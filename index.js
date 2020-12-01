edff9bf8f72e90244c66d8dc67aa3c2717a71c7a
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-01 01:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(rightNumbers, 10);
 
-  if (left > 0) {
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
@@ -18,7 +18,7 @@ function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left < 360) {
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
 }
